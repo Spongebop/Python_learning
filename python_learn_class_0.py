4cc6dfef531fc9581431031fb745965e1d0de2c0
Spongebop <42912468+Spongebop@users.noreply.github.com>
2018-11-19 13:26:36 +0800

    Add files via upload

--- python_learn_class_0.py
+++ python_learn_class_0.py
@@ -5,6 +5,7 @@ class Restautant():
         """初始化"""
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
+        self.number_servedd=0
 
     def describe_resturant(self) :
         """打印前两个餐馆消息"""
@@ -13,7 +14,17 @@ class Restautant():
     def open_resturant(self):
         """说出现在餐厅是否在开业状态中"""
         print("目前餐厅在开业状态")
+    def number_served(self):
+        """反馈出有多少人就餐"""
+        print("这里已经有"+str(self.number_servedd)+"就餐过了。")
 
+    def set_number_server(self,number):
+        """重写就餐人数"""
+        self.number_servedd=number
+
+    def increamenr_number_served(self,add_number):
+        """增加每天来的客人数目"""
+        self.number_servedd+=add_number
 
 resturant_0= Restautant("shagou",'kuaican')
 resturant_0.open_resturant()
@@ -24,3 +35,14 @@ resturant_1.describe_resturant()
 
 resturant_2=Restautant('华莱士','外卖')
 resturant_2.describe_resturant()
+
+
+resturant=Restautant('zhangjiekairou','kuacan')
+resturant.set_number_server(23)
+resturant.increamenr_number_served(2)
+resturant.number_served()
+resturant.describe_resturant()
+
+
+
+
